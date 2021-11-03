389adf5c59c8ada1a22c060a88b6a99668dbe84c
PaoloGio <s290176@studenti.polito.it>
2021-11-03 19:07:01 +0100

    folder main added

---
+++ Lab_1/main/c/check_result.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv)
+{
+    //results check
+
+    if (argc == 3)   //check if there is the name of the file to be compared
+    {
+
+    FILE * fp_simresults_file = fopen(argv[1], "r"); //open file to be checked
+        if ( fp_simresults_file  == NULL)
+        {
+            printf("Error: cannot open %s\n", argv[1]);
+            exit(2);
+        }
+
+        FILE* fp_reference_file = fopen(argv[2], "r"); //open file with the c results
+        int res_sim, res_c;
+        int error = 0;
+        int line = 0;
+
+        fscanf(fp_simresults_file, "%d\n", &res_sim);  //read first data from vhdl file
+        do
+        {
+            fscanf(fp_reference_file, "%d\n", &res_c);
+            line++;
+            if (res_sim != res_c)
+            {
+                printf("Error at line %d:\nC result:%d\nsimulation result:%d\n", line, res_c, res_sim);
+                error = 1;
+            }
+            fscanf(fp_simresults_file, "%d\n", &res_sim);
+        }
+        while (!feof(fp_simresults_file) && !feof(fp_reference_file) );
+
+        if (error == 1)
+            printf("Check failed!\n");
+        else
+            printf("Check successful!\n");
+
+        fclose(fp_reference_file);
+        fclose(fp_simresults_file);
+
+    }
+    else{
+        printf("Use: %s <input_file> <output_file> <check_flag>\n", argv[0]);
+    }
+
+    return 0;
+}
---
+++ Lab_1/main/matlab/my_fir_filter.m
@@ -0,0 +1,75 @@
+
+%% BE CAREFUL!
+% if you run this file, the random sequence of vin will change and you will
+% have to run again the C program and the vhdl to obtain coherent results
+
+fs=10000; %% sampling frequency
+f1=500;  %% first sinewave freq (in band)
+f2=4500; %% second sinnewave freq (out band)
+f3=1200; %% square root freq
+
+p=1;
+x_in=5;
+y_in=8;
+N=(2^p)*(mod(x_in,2)+1)+6*p; %% filter order
+nb=mod(y_in,7)+8; %% number of bits
+
+T=1/500; %% maximum period
+tt=0:1/fs:10*T; %% time samples
+
+x1=sin(2*pi*f1*tt); %% first sinewave
+x2=sin(2*pi*f2*tt); %% second sinewave
+
+%%%%    CHOOSE HERE between sinewave and worst case square wave  %%%%%%%
+x=(x1+x2)/2; %% input signal
+%x=square(2*pi*f3*tt,40);
+
+[bi, bq]=myfir_design(N, nb); %% filter design
+
+y=filter(bq, 1, x); %% apply filter
+
+%% plots
+figure
+plot(tt,x1,'--d');
+hold on
+plot(tt,x2,'r--s');
+plot(tt,x, 'g--+');
+plot(tt, y, 'c--o');
+
+legend('x1', 'x2', 'x', 'y')
+
+%% quantize input and output
+%moving to integer numbers represented on nb bits 
+%x samples with value 1 can get overflow, so its corresponding value 
+%is changed to 2^(nb-1)-1
+%output range from -2^(nb-1) to 2^(nb-1)-1
+xq=floor(x*2^(nb-1));
+idx=find(xq==2^(nb-1));
+xq(idx)=2^(nb-1)-1;
+
+yq=floor(y*2^(nb-1));
+idy=find(yq==2^(nb-1));
+yq(idy)=2^(nb-1)-1;
+
+
+%% save input and output
+fp=fopen('samples.txt','w');
+
+%%for vin, generate random value 0 with probability 10% and 1 with 90%
+r1 = rand(1,length(xq));
+vin = zeros(1,length(xq));
+
+for i = 1:1:length(xq)
+    if r1(i) > 0.97
+        vin = 0;
+    else
+        vin = 1;
+    end 
+    fprintf(fp,'%d %d\n', xq(i), vin);
+end ;
+
+fp=fopen('resultsm.txt', 'w');
+fprintf(fp, '%d\n', yq);
+fclose(fp);
+
+
