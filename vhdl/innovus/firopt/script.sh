a7ba54ae64d9af3290ca259bec468a10a1ae05df
PaoloGio <s290176@studenti.polito.it>
2021-11-16 22:32:51 +0100

    .gitignore file

--- vhdl/innovus/firopt/script.sh
+++
@@ -1,7 +0,0 @@
-#option to use pattern list
-shopt -s extglob
-rm -rfv !("design.globals"|"innovus.cmd"| "pow_consum.cmd" |"script.sh"|"mmm_design.tcl")
-
-source /software/scripts/init_innovus17.11 
-innovus
-
